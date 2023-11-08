76ba6e2ab7413dfeaccd7690b099f8349ee57219
Marcus Rådell <marcus@radell.net>
2023-11-08 19:24:36 +0100

    add database call to get the luck value

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,5 +1,7 @@
-use axum::{routing::get, Json, Router};
-use libsql_client::client::Client;
+use std::sync::Arc;
+
+use axum::{extract::State, routing::get, Json, Router};
+use libsql_client::{client::Client, Value};
 use shuttle_secrets::SecretStore;
 use tower_http::cors::CorsLayer;
 
@@ -8,8 +10,29 @@ struct LuckResponse {
     luck: bool,
 }
 
-async fn luck() -> impl axum::response::IntoResponse {
-    Json(LuckResponse { luck: true })
+async fn luck_handler(state: State<AppState>) -> impl axum::response::IntoResponse {
+    let db_result = state.db.execute("select * from luck").await.unwrap();
+
+    let row = db_result.rows.get(0).unwrap();
+
+    let value = row.values.get(1).unwrap();
+
+    let luck = match value {
+        Value::Integer { value } => {
+            if *value == 1 {
+                true
+            } else {
+                false
+            }
+        }
+        _ => false,
+    };
+    Json(LuckResponse { luck })
+}
+
+#[derive(Clone)]
+struct AppState {
+    db: Arc<Client>,
 }
 
 #[shuttle_runtime::main]
@@ -20,12 +43,22 @@ async fn main(
         token = "{secrets.DB_TURSO_TOKEN}"
     )]
     client: Client,
-    // use secrets if you are not hardcoding your token/addr
-    #[shuttle_secrets::Secrets] secrets: SecretStore,
 ) -> shuttle_axum::ShuttleAxum {
+    // client
+    //     .execute("create table luck (uid text primary key, luck integer not null)")
+    //     .await?;
+
+    // client
+    //     .execute("insert into luck (uid, luck) values ('marcus', 1)")
+    //     .await?;
+    let app_state = AppState {
+        db: Arc::new(client),
+    };
+
     let router = Router::new()
-        .route("/api/luck", get(luck))
-        .layer(CorsLayer::permissive());
+        .route("/api/luck", get(luck_handler))
+        .layer(CorsLayer::permissive())
+        .with_state(appState);
 
     Ok(router.into())
 }
