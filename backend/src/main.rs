91ec919676a80615f20b5ee21f4c3c0fd614bc6b
Marcus Rådell <marcus@radell.net>
2023-11-08 18:49:50 +0100

    setup turso client

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,4 +1,6 @@
 use axum::{routing::get, Json, Router};
+use libsql_client::client::Client;
+use shuttle_secrets::SecretStore;
 use tower_http::cors::CorsLayer;
 
 #[derive(serde::Serialize)]
@@ -11,7 +13,16 @@ async fn luck() -> impl axum::response::IntoResponse {
 }
 
 #[shuttle_runtime::main]
-async fn main() -> shuttle_axum::ShuttleAxum {
+
+async fn main(
+    #[shuttle_turso::Turso(
+        addr = "libsql://lab-next-bff-marcusradell.turso.io",
+        token = "{secrets.DB_TURSO_TOKEN}"
+    )]
+    client: Client,
+    // use secrets if you are not hardcoding your token/addr
+    #[shuttle_secrets::Secrets] secrets: SecretStore,
+) -> shuttle_axum::ShuttleAxum {
     let router = Router::new()
         .route("/api/luck", get(luck))
         .layer(CorsLayer::permissive());
