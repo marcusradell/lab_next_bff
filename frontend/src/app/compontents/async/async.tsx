496c930ca9216cbf3f3cea6a38037f2ee816df78
Marcus Rådell <marcus@radell.net>
2023-11-08 22:26:43 +0100

    update animation speed

--- frontend/src/app/compontents/async/async.tsx
+++ frontend/src/app/compontents/async/async.tsx
@@ -34,7 +34,10 @@ export const Async: FC<Props> = ({ status, render }) => {
   if (!isDone)
     return (
       <div
-        className={"absolute top-1/2 left-1/2 animate__animated " + animation}
+        className={
+          "absolute top-1/2 left-1/2 animate__animated animate__fast " +
+          animation
+        }
       >
         <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-white"></div>
       </div>
