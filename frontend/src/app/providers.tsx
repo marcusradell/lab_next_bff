7bb76607d2b4667a59e205c0a33cd75fbb995c3f
Marcus Rådell <marcus@radell.net>
2023-11-08 12:59:38 +0100

    setup react query client provider

--- frontend/src/app/providers.tsx
+++ frontend/src/app/providers.tsx
@@ -1,9 +1,14 @@
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { FC } from "react";
 
 type Props = {
   children: React.ReactNode;
 };
 
+const client = new QueryClient();
+
 export const Providers: FC<Props> = ({ children }) => {
-  return <>{children}</>;
+  return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
 };
