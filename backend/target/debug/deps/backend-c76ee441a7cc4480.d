a72f3e7f1f5264e6600f2a3c291a21d42b8c901b
Marcus Rådell <marcus@radell.net>
2023-11-08 09:57:32 +0100

    setup monorepo structure

---
+++ backend/target/debug/deps/backend-c76ee441a7cc4480.d
@@ -0,0 +1,5 @@
+/Users/marcusradell/code/marcusradell/lab_next_bff/backend/target/debug/deps/backend-c76ee441a7cc4480.rmeta: src/main.rs
+
+/Users/marcusradell/code/marcusradell/lab_next_bff/backend/target/debug/deps/backend-c76ee441a7cc4480.d: src/main.rs
+
+src/main.rs:
