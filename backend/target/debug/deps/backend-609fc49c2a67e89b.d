e50ee5235cdfc5a2fb37c00c41d4c157ed836b08
Marcus Rådell <marcus@radell.net>
2023-11-08 13:24:19 +0100

    drop backend code to prep for shuttle init

--- backend/target/debug/deps/backend-609fc49c2a67e89b.d
+++
@@ -1,5 +0,0 @@
-/Users/marcusradell/code/marcusradell/lab_next_bff/backend/target/debug/deps/backend-609fc49c2a67e89b.rmeta: src/main.rs
-
-/Users/marcusradell/code/marcusradell/lab_next_bff/backend/target/debug/deps/backend-609fc49c2a67e89b.d: src/main.rs
-
-src/main.rs:
--- backend/target/debug/deps/backend-c76ee441a7cc4480.d
+++
@@ -1,5 +0,0 @@
-/Users/marcusradell/code/marcusradell/lab_next_bff/backend/target/debug/deps/backend-c76ee441a7cc4480.rmeta: src/main.rs
-
-/Users/marcusradell/code/marcusradell/lab_next_bff/backend/target/debug/deps/backend-c76ee441a7cc4480.d: src/main.rs
-
-src/main.rs:
