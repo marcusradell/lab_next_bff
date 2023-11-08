49c4df40a966699ac5df5fd362c1b42d8efcf191
Marcus Rådell <marcus@radell.net>
2023-11-08 21:20:44 +0100

    get new cours plan data

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,14 +2,13 @@
 
 import { useQuery } from "@tanstack/react-query";
 
-const luckyReplyMessage = (lucky: boolean) => (lucky ? "Yes" : "No");
-
 const baseUrl = process.env.NEXT_PUBLIC_API_URL;
 
 export default function Home() {
   const { status, data, error } = useQuery({
-    queryKey: ["luck"],
-    queryFn: () => fetch(`${baseUrl}/luck`).then((res) => res.json()),
+    queryKey: ["course_plans"],
+    queryFn: () =>
+      fetch(`${baseUrl}/course_plans/get_all`).then((res) => res.json()),
   });
 
   if (status === "pending") {
@@ -21,8 +20,6 @@ export default function Home() {
     return <div>Something went wrong.</div>;
   }
 
-  const luckyString = luckyReplyMessage(data);
-
   return (
     <main className="container m-auto grid grid-cols-12">
       <h1 className="col-span-full text-4xl font-bold text-center p-4">
@@ -33,7 +30,7 @@ export default function Home() {
       </div>
 
       <div className="text-2xl col-span-full text-center p4 animate__animated animate__zoomIn">
-        {luckyString}
+        <pre>{JSON.stringify(data, null, 2)}</pre>
       </div>
     </main>
   );
