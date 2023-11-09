dc31193c77267e92cd5c3df8f6e2c52b992ca89e
Marcus Rådell <marcus@radell.net>
2023-11-09 13:40:30 +0100

    set port 8080

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -17,7 +17,7 @@ async fn main() {
 
     // run our app with hyper
     // `axum::Server` is a re-export of `hyper::Server`
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
     tracing::debug!("listening on {}", addr);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
